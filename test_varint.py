bed8e5926b1227ea6dd0fb777439d7764830dd24
Developer <devel@sa.cs.ut.ee>
2016-09-28 12:45:20 +0300

    Modified file permissions, re-produced Kurwa-Bug

--- test_varint.py
+++ test_varint.py
@@ -20,4 +20,13 @@ if __name__ == '__main__':
     f.seek(0)
     assert varint.test_file_seek(f, 0) == 'A'
     assert f.tell() == 1
+    f.seek(0)
+    assert f.read(1) == 'A'
+    assert varint.test_file_seek(f,0) == 'B'
+# TODO: Kurwa bug:
+# If we issue seek once more after calling read from C varint.test_file_seek,
+# the seek pointer gets reset to 0 (inplace of +1) 
+    f.seek(1)
+    assert varint.test_file_seek(f,0) == 'D'
+    assert f.read(1) == 'E'
     f.close()
