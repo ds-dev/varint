89a0fa66ae1ec656748a84a8e4d139faff74cc7d
Alexander Tkachenko <alex.tk.fb@gmail.com>
2016-09-30 22:43:59 +0300

    updated tests

--- test.py
+++ test.py
@@ -6,46 +6,41 @@ import os
 class TestVarint(unittest.TestCase):
     def setUp(self):
         self.test_file = '/tmp/varint-test.txt'
-        f = open(self.test_file, 'w')
-        f.close()
 
     def tearDown(self):
         os.remove(self.test_file)
 
     def test_size(self):
-        f = open(self.test_file, 'r+b')
+        with open(self.test_file, 'w+b') as f:
+            # test one byte
+            varint.varint_write(f, 2 ** 0)
+            self.assertEqual(f.tell(), 1)
+            f.seek(0)
 
-        # test one byte
-        varint.varint_write(f, 2 ** 0)
-        self.assertEqual(f.tell(), 1)
-        f.seek(0)
+            varint.varint_write(f, 2 ** 6)
+            self.assertEqual(f.tell(), 1)
+            f.seek(0)
 
-        varint.varint_write(f, 2 ** 6)
-        self.assertEqual(f.tell(), 1)
-        f.seek(0)
+            # test two bytes
+            varint.varint_write(f, 2 ** 7)
+            self.assertEqual(f.tell(), 2)
+            f.seek(0)
 
-        # test two bytes
-        varint.varint_write(f, 2 ** 7)
-        self.assertEqual(f.tell(), 2)
-        f.seek(0)
+            varint.varint_write(f, 2 ** 14)
+            self.assertEqual(f.tell(), 2)
+            f.seek(0)
 
-        varint.varint_write(f, 2 ** 14)
-        self.assertEqual(f.tell(), 2)
-        f.seek(0)
+            # test three byte
+            varint.varint_write(f, 2 ** 15)
+            self.assertEqual(f.tell(), 3)
+            f.seek(0)
 
-        # test three byte
-        varint.varint_write(f, 2 ** 15)
-        self.assertEqual(f.tell(), 3)
-        f.seek(0)
-
-        varint.varint_write(f, 2 ** 21)
-        self.assertEqual(f.tell(), 3)
-        f.seek(0)
-
-        f.close()
+            varint.varint_write(f, 2 ** 21)
+            self.assertEqual(f.tell(), 3)
+            f.seek(0)
 
     def test_rw(self):
-        with open(self.test_file, 'r+b') as f:
+        with open(self.test_file, 'w+b') as f:
             for pw in range(31):
                 n = 2 ** pw
                 varint.varint_write(f, n)
