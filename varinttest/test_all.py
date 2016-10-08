cce0d0f7eabe401f3fa7e5bbed82424a93d6fcf8
Developer <devel@sa.cs.ut.ee>
2016-10-08 14:06:00 +0300

    Added Simple test compantible to 2x and 3x

---
+++ varinttest/test_all.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import os
+from sys import version_info
+import varint
+ 
+if __name__ == '__main__':
+    test_ns = [42,4353,33,666,99,3443]
+    test_file = '/tmp/varint-test.txt'
+
+    # write first
+    f = open(test_file,'w+b')
+    for n in test_ns:
+        varint.varint_write(f,n)
+    f.flush()
+    f.close()
+
+    # check if written
+    f = open(test_file,'r+b')
+    for exp in test_ns:
+        n = varint.varint_read(f)
+        assert(n == exp)
+    f.close()
+
+    # remove test file
+    os.remove(test_file)
