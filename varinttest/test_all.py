160b9293c7dbc41cfecaa4259508735e8bc4b3c2
Developer <devel@sa.cs.ut.ee>
2016-11-07 12:40:36 +0200

    Customized for testing Python3 VarIntRead

--- varinttest/test_all.py
+++ varinttest/test_all.py
@@ -8,24 +8,25 @@ import varint
 # Also looks like issueing fflush from C did
 # solve the seek issue
 # the buffering=0 is not anymore critical
- 
+
 if __name__ == '__main__':
     test_ns = [42,4353,33,666,99,3443]
     test_file = '/tmp/varint-test.txt'
 
     # write first
-    f = open(test_file,'w+b')
-    for n in test_ns:
-        varint.varint_write(f,n)
-    f.flush()
-    f.close()
+    #f = open(test_file,'w+b')
+    #for n in test_ns:
+    #    varint.varint_write(f,n)
+    #f.flush()
+    #f.close()
 
     # check if written
     f = open(test_file,'r+b')
     for exp in test_ns:
         n = varint.varint_read(f)
+        print ('Expected ' + str(exp) + ' got ' + str(n))
         assert(n == exp)
     f.close()
 
     # remove test file
-    os.remove(test_file)
+    #os.remove(test_file)
